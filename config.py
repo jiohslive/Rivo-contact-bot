3ca997e1e4ea80ee88f00fb2d9d2f800d65d2913
jiohslive <yashpisal8353@gmail.com>
2026-02-22 17:54:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
+import os
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+
+WELCOME_TEXT = (
+    "👋 Hello {name} and welcome to the Bot!\n\n"
+    "🛠️ Created by <b>Rivo Bots</b>\n"
+    "🔗 Channel: <a href='https://t.me/RivoBots'>@RivoBots</a>\n\n"
+    "⬇️ Write a message here and you’ll receive a reply as soon as possible."
+)
