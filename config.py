ad123c503e8be4d6893a508b527899ca846664da
jiohslive <yashpisal8353@gmail.com>
2026-02-22 18:28:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
 
 WELCOME_TEXT = (
     "👋 Hello {name} and welcome to the Bot!\n\n"
-    "Created By <a href='https://t.me/RivoBots'>RivoBots</a>\n\n"
+    "🧑🏻‍💻 Created By <a href='https://t.me/RivoBots'>RivoBots</a>\n\n"
     "⬇️ <u>Write a message here</u> and you’ll receive a reply as soon as possible."
 )
