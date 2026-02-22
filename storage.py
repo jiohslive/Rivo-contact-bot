67490abe8da274ee8f6f54326b7b98fefb0276d5
jiohslive <yashpisal8353@gmail.com>
2026-02-22 17:54:44 +0530

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,1 +1,17 @@
+import os
+import json
 
+USERS_FILE = "users.json"
+
+def load_users():
+    if not os.path.exists(USERS_FILE):
+        return []
+    with open(USERS_FILE, "r") as f:
+        return json.load(f)
+
+def save_user(user_id: int):
+    users = load_users()
+    if user_id not in users:
+        users.append(user_id)
+        with open(USERS_FILE, "w") as f:
+            json.dump(users, f)
