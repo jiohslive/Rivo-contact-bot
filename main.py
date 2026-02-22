dcd43c4893c977187637e06cc02d3dbfb420c3f4
jiohslive <yashpisal8353@gmail.com>
2026-02-22 17:56:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 
+from config import BOT_TOKEN
+from handlers import start, user_message, admin_reply
+
+def main():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND & ~filters.REPLY, user_message))
+    app.add_handler(MessageHandler(filters.REPLY & filters.TEXT, admin_reply))
+
+    print("🤖 Bot running...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
